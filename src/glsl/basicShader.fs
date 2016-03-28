8cbe0d97c42f10c40cbb2165bbb63b664fdf6e7a
Bruno da Silva Belo <brunodasilvabelo@gmail.com>
2016-03-28 08:53:57 -0300

    save

--- src/glsl/basicShader.fs
+++ src/glsl/basicShader.fs
@@ -1,34 +1,90 @@
 #version 330 core
 
+struct Material {
+  sampler2D diffuse;
+  sampler2D specular;
+  float shininess;
+};
+
+struct DirectionLight {
+  vec3 direction;
+
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+};
+
+struct PointLight {
+  vec3 position;
+
+  float constant;
+  float linear;
+  float quadratic;
+
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+};
+
+struct Light {
+  vec3 position;
+  vec3 direction;
+  float cutOff;
+  float outerCutOff;
+
+  float constant;
+  float linear;
+  float quadratic;
+
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+};
+
 in vec3 normal0;
 in vec3 fragPosition;
 in vec2 texCoord0;
 
 out vec4 color;
 
-uniform sampler2D sampler;
-uniform vec3 lightColor;
-uniform vec3 objectColor;
-uniform vec3 lightPosition;
+uniform float test[MAX];
 uniform vec3 viewPosition;
+uniform Material material;
+uniform Light light;
 
 void main() {
   // ambient
-  float ambientStrength = 0.1f;
-  vec3 ambient = ambientStrength * lightColor;
+  vec3 ambient = light.ambient * vec3(texture(material.diffuse, texCoord0));
 
   // diffuse
   vec3 norm = normalize(normal0);
-  vec3 lightDirection = normalize(lightPosition - fragPosition);
+  vec3 lightDirection = normalize(light.direction - fragPosition);
   float diff = max(dot(norm, lightDirection), 0.f);
-  vec3 diffuse = diff * lightColor;
+  vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, texCoord0));
 
   // specular
-  float specularStrength = 0.5f;
   vec3 viewDirection = normalize(viewPosition - fragPosition);
   vec3 reflectDirection = reflect(-lightDirection, norm);
-  float spec = pow(max(dot(viewDirection, reflectDirection), 0.f), 32);
-  vec3 specular = specularStrength * spec * lightColor;
+  float spec = pow(max(dot(viewDirection, reflectDirection), 0.f),
+                   material.shininess);
+  vec3 specular = light.specular * spec
+    * vec3(texture(material.specular, texCoord0));
 
-  color = texture2D(sampler, texCoord0) * vec4((ambient + diffuse + specular) * objectColor, 1.f);
+  // Spotlight (soft edges)
+  float theta = dot(lightDirection, normalize(-light.direction));
+  float epsilon = (light.cutOff - light.outerCutOff);
+  float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.f, 1.f);
+
+  diffuse *= intensity;
+  specular *= intensity;
+
+  // Attenuation
+  float dist = length(light.position - fragPosition);
+  float attenuation = 1.f / (light.constant + light.linear * dist
+                             + light.quadratic * (dist * dist));
+  ambient *= attenuation;
+  diffuse *= attenuation;
+  specular *= attenuation;
+
+  color = vec4(ambient + diffuse + specular, 1.f);
 }
