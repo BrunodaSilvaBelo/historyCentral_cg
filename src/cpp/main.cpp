90bb20daaeb04dd0863701e0b7f022a514d2a28f
Bruno da Silva Belo <brunodasilvabelo@gmail.com>
2016-04-11 11:58:50 -0300

    Projeto terminado com a porta

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -2,7 +2,7 @@
    @mainpage
    @brief
    @author Bruno da Silva Belo
-   @bug Luz sÃ³ funciona se existir uma directional light
+   @bug Luz só funciona se existir uma directional light
    @bug Directional light parou de funcionar
 */
 
@@ -22,21 +22,35 @@
 
 using namespace std;
 
+void automaticDoor(Transform &transform, const Camera &camera);
+
+void automaticDoor(Transform &transform, const Camera &camera) {
+  static float rotate = 0.f;
+  float direction = 1.f;
+
+  if (glm::distance(transform.getPosition(), camera.getPosition()) >= 5.f)
+    direction = -direction;
+
+  rotate += Window::deltaTime() * direction;
+  rotate = (rotate <= glm::radians(0.f)) ? glm::radians(0.f) : rotate;
+  rotate = (rotate >= glm::radians(90.f)) ? glm::radians(90.f) : rotate;
+
+  transform.applyRotation({0.f, rotate, 0.f});
+}
+
 int main() {
   try {
     Window::init("OpenGL", 800, 600);
 
     Shader shader("../src/glsl/basicShader");
     Camera camera(glm::vec3(0.f, 0.f, 3.f), 70.f, Window::aspect(), 0.1f, 100.f);
-    Model church("../res/models/church/church.obj");
-    Model nano("../res/models/nanosuit/nanosuit.obj");
+
     Transform transform;
     shader.bind();
     shader.update(Shader::PROJECTION, camera.getProjection());
 
     DirectionalLight dLight({0.f,0.f, 1.f});
     dLight.init(shader);
-    printf("%s\n", dLight.to_string().c_str());
 
     vector<glm::vec3> lightPositions = {
       {0.f,0.f,-2.f},
@@ -64,9 +78,11 @@ int main() {
     };
 
     vector<Model> models = {
-      church,
-      nano,
+      Model("../res/models/church/church.obj"),
+      Model("../res/models/nanosuit/nanosuit.obj")
     };
+
+    Model door("../res/models/church/door.obj");
 
     Window::startTimer();
     while (!Window::isClosed()) {
@@ -83,10 +99,17 @@ int main() {
 
       for (unsigned i = 0; i < models.size(); ++i) {
         transform.applyTranslate(positions[i]);
+        transform.applyRotation({0.f,0.f,0.f});
         transform.applyScale(scales[i]);
         shader.update(Shader::MODEL, transform.getModel());
         models[i].draw(shader);
       }
+
+      transform.applyTranslate(churchPosition + glm::vec3(-0.27f,-2.34f, 1.1f));
+      transform.applyScale(glm::vec3(1.f));
+      automaticDoor(transform, camera);
+      shader.update(Shader::MODEL, transform.getModel());
+      door.draw(shader);
 
       Window::update();
       counter += 0.01f;
--- src/glsl/basicShader.fs
+++ src/glsl/basicShader.fs
@@ -1,5 +1,12 @@
 #version 330 core
 
+// POST_PROCESSING
+#define INVERSION 0
+#define GRAYSCALE 0
+#define SEPIA 0
+#define KERNEL 0
+#define SOBEL                        // SHARPEN, BLUR, EDGE, SOBEL, LEFT_SOBEL
+
 struct Material {
   sampler2D texture_diffuse1;
   sampler2D texture_specular1;
@@ -53,14 +60,16 @@ uniform int numPointLight;
 uniform PointLight pointLight[10];
 uniform SpotLight spotLight;
 
+#if !KERNEL
 vec3 calcBaseLight(BaseLight light, vec3 lightDirection, vec3 normal);
 vec3 calcDirectionalLight(DirectionalLight light, vec3 normal);
 vec3 calcPointLight(PointLight light, vec3 normal);
 vec3 calcSpotLight(SpotLight light, vec3 normal);
 
 vec3 calcBaseLight(BaseLight light, vec3 lightDirection, vec3 normal) {
-  vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1,
-                                              texCoord0));
+  vec3 ambient = light.ambient *
+    vec3(texture(material.texture_diffuse1, texCoord0));
+
   vec3 diffuse = vec3(0.f);
   vec3 specular = vec3(0.f);
 
@@ -117,7 +126,6 @@ void main() {
   vec3 normal = normalize(normal0);
 
   vec3 result = vec3(0.f);
-
   result += calcDirectionalLight(directionalLight, normal);
 
   for (int i = 0; i < numPointLight; ++i)
@@ -127,4 +135,153 @@ void main() {
     result += calcSpotLight(spotLight, normal);
 
   color = vec4(result, 1.f);
+
+#if GRAYSCALE
+  float avg = 0.2126f * color.r + 0.7152 * color.g + 0.0722 * color.b;
+  color = vec4(vec3(avg), 1.f);
+#endif
+#if INVERSION
+  color = vec4(1-result, 1.f);
+#endif
+#if SEPIA
+  vec4 sepiaColor = vec4(
+                         clamp(color.r * 0.393f + color.g * 0.769f + color.b * 0.189f, 0.f, 1.f),
+                         clamp(color.r * 0.349f + color.g * 0.686f + color.b * 0.168f, 0.f, 1.f),
+                         clamp(color.r * 0.272f + color.g * 0.534f + color.b * 0.131f, 0.f, 1.f),
+                         color.a);
+  color = mix(color, sepiaColor, 1.f);
+#endif
 }
+#else
+vec3 calcBaseLight(BaseLight light, vec3 lightDirection, vec3 normal, vec2 offset);
+vec3 calcDirectionalLight(DirectionalLight light, vec3 normal, vec2 offset);
+vec3 calcPointLight(PointLight light, vec3 normal, vec2 offset);
+vec3 calcSpotLight(SpotLight light, vec3 normal, vec2 offset);
+
+vec3 calcBaseLight(BaseLight light, vec3 lightDirection, vec3 normal, vec2 offset) {
+  vec3 ambient = light.ambient *
+    vec3(texture(material.texture_diffuse1, texCoord0.st + offset));
+
+  vec3 diffuse = vec3(0.f);
+  vec3 specular = vec3(0.f);
+
+  float diff = dot(normal, -lightDirection);
+  if (diff > 0) {
+    diffuse = diff * (light.diffuse
+                      * vec3(texture(material.texture_diffuse1,
+                                     texCoord0.st + offset)));
+
+    vec3 viewDirection = normalize(viewPosition - fragPosition);
+    vec3 reflectDirection = normalize(reflect(lightDirection, normal));
+    float spec = dot(viewDirection, reflectDirection);
+    if (spec > 0) {
+      spec = pow(spec, material.shininess);
+      specular = spec * (light.specular
+                         * vec3(texture(material.texture_specular1,
+                                        texCoord0.st + offset)));
+    }
+  }
+
+  return (ambient + diffuse + specular);
+}
+
+vec3 calcDirectionalLight(DirectionalLight light, vec3 normal, vec2 offset) {
+  return calcBaseLight(light.base, light.direction, normal, offset);
+}
+
+vec3 calcPointLight(PointLight light, vec3 normal, vec2 offset) {
+  vec3 lightDirection = fragPosition - light.position;
+  float dist = length(lightDirection);
+  lightDirection = normalize(lightDirection);
+
+  vec3 result = calcBaseLight(light.base, lightDirection, normal, offset);
+  float attenuation = 1.f / (light.attenuation.constant
+                             + light.attenuation.linear * dist
+                             + light.attenuation.quadratic * dist * dist);
+
+  return attenuation * result;
+}
+
+vec3 calcSpotLight(SpotLight light, vec3 normal, vec2 offset) {
+  vec3 lightDirection = normalize(fragPosition - light.base.position);
+  float theta = dot(lightDirection, light.direction);
+
+  if (theta < light.cutOff)
+    return vec3(0.f);
+
+  vec3 result = calcPointLight(light.base, normal, offset);
+  float epsilon = light.cutOff - light.outerCutOff;
+  float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.f, 1.f);
+
+  return intensity * result;
+}
+
+void main() {
+  vec3 normal = normalize(normal0);
+
+  const float offset = 1.f/300.f;
+  const vec2 offsets[9] = vec2[](
+                                 vec2(-offset, offset),  // top-left
+                                 vec2(0.0f,    offset),  // top-center
+                                 vec2(offset,  offset),  // top-right
+                                 vec2(-offset, 0.0f),    // center-left
+                                 vec2(0.0f,    0.0f),    // center-center
+                                 vec2(offset,  0.0f),    // center-right
+                                 vec2(-offset, -offset), // bottom-left
+                                 vec2(0.0f,    -offset), // bottom-center
+                                 vec2(offset,  -offset)  // bottom-right
+                                 );
+
+#ifdef SHARPEN
+  float kernel[9] = float[](
+                            -1, -1, -1,
+                            -1,  5, -1,
+                            -1, -1, -1
+                            );
+#endif
+#ifdef BLUR
+  float kernel[9] = float[](
+                            1.f/16.f, 2.f/16.f, 1.f/16.f,
+                            2.f/16.f, 4.f/16.f, 2.f/16.f,
+                            1.f/16.f, 2.f/16.f, 1.f/16.f
+                            );
+#endif
+#ifdef EDGE
+  float kernel[9] = float[](
+                            1, 1, 1,
+                            1, -8, 1,
+                            1, 1, 1
+                            );
+#endif
+#ifdef LEFT_SOBEL
+  float kernel[9] = float[](
+                            1, 0, -1,
+                            2, 0, -2,
+                            1, 0, -1
+                            );
+#endif
+#ifdef SOBEL
+  float kernel[9] = float[](
+                            -2, -1, 0,
+                            -1, 1, 1,
+                            0, 1, 2
+                            );
+#endif
+  vec3 sampleTex[9];
+  for (int i = 0; i < 9; ++i)
+    sampleTex[i] = calcDirectionalLight(directionalLight, normal, offsets[i]);
+
+  for (int i = 0; i < 9; ++i)
+    for (int j = 0; j < numPointLight; ++j)
+      sampleTex[i] += calcPointLight(pointLight[j], normal, offsets[i]);
+
+  for (int i = 0; i < 9; ++i)
+    sampleTex[i] += calcSpotLight(spotLight, normal, offsets[i]);
+
+  vec3 col = vec3(0.f);
+  for (int i = 0; i < 9; ++i)
+    col += sampleTex[i] * kernel[i];
+
+  color = vec4(col, 1.f);
+}
+#endif
